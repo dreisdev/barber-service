c04afc01afcc1868a642dbc726bbe5085af6766b
dreisdev <dreis.dev@outlook.com>
2024-03-07 18:39:32 -0300

    implemented create queue list today

--- src/queues/queues.controller.ts
+++ src/queues/queues.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, HttpStatus, NotFoundException, Post, Res } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpStatus, NotFoundException, Post, Query, Res } from '@nestjs/common';
 import { QueuesService } from './queues.service';
 import CreateQueueDto from './dtos/create-queue';
 import { Response } from 'express';
@@ -32,5 +32,34 @@ export class QueuesController {
 
     const queue = await this.queuesService.createQueue(data)
     return res.status(HttpStatus.CREATED).json(queue)
+  }
+
+  @Get()
+  async getExpertQueues(
+    @Query('expertId') expertId: string,
+    @Res() res: Response
+  ) {
+    if (expertId) {
+
+      const expert = await this.expertsService.findExpert(expertId)
+
+      if (!expert) {
+        throw new NotFoundException('O expert não existe')
+      }
+
+      const queues = await this.queuesService.getExpertQueues(expertId)
+      return res.status(HttpStatus.OK).json(queues)
+    }
+
+    const queues = await this.queuesService.getQueues()
+    return res.status(HttpStatus.OK).json(queues)
+  }
+
+  @Get('/today')
+  async getQueuesToday(
+    @Res() res: Response
+  ) {
+    const queues = await this.queuesService.getQueuesToday()
+    return res.status(HttpStatus.OK).json(queues)
   }
 }
